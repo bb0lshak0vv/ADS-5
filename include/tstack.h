2bd4d8aa59cf0e8834cc574b6f1e86c86f0b3b20
bb0lshak0vv <58132397+bb0lshak0vv@users.noreply.github.com>
2023-04-10 00:10:09 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,41 +2,46 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 #include <string>
-template<typename T, int size>
+
+template <typename T, int size>
 class TStack {
+  // добавьте код стека
  private:
-    T arr[100];
-    int top;
+  T arr[size] = {0};
+  int top = 0;
 
  public:
-    Tstack() :top(-1) {}
-
-    void push(T value) {
-      if (isFull()) {
-        throw std::string("Full!");
-      } else {
-        arr[++top] = value;
-      }
+  TStack(): top(-1) {}
+  void push(const T& value) {
+     if (isFull())
+       throw std::string("Full");
+     else
+       arr[++top] = value;
     }
-
-      const T& pop() {
-      if (isEmpty()) {
-        throw std::string("Empty!");
-      } else {
+    const T& pop() {
+      if (isEmpty())
+        throw std::string("Empty");
+      else
         return arr[top--];
-      }
     }
-
-    bool isEmpty()const {
+    const T& get() const {
+      if (isEmpty())
+        throw std::string("Empty");
+      else
+        return arr[top];
+    }
+    bool isEmpty() const {
       return top == -1;
     }
-
-    bool isFull()const {
+    bool isFull() const {
       return top == size - 1;
     }
-
-    const T& get()const {
-      return arr[top];
+    const T& GetArr(int i) {
+      return arr[i];
+    }
+    int GetTop() {
+      return top;
     }
 };
+
 #endif  // INCLUDE_TSTACK_H_
